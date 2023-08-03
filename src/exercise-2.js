e7495c243e4dfa85680c1f65661f6711560cd288
Daphne Balane <balanedaphne20@gmail.com>
2023-08-03 17:19:07 +0800

    (JavaScript-exam): Added source code in exercise-1, exercise-2, and exercise-3 JS files

--- src/exercise-2.js
+++ src/exercise-2.js
@@ -1,5 +1,9 @@
 const getTheExactNumber = (numbers) => {
-  // implement code here
+  const getFilteredNumbers = numbers.filter(number => number % 3 === 0);
+    if (getFilteredNumbers.length === 0) {
+        return null;
+    }
+    return Math.max(...getFilteredNumbers);
 }
 
 export default getTheExactNumber;
